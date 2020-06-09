6f455ac95435fa50433330eeab3319041a92bbbb
benlex5 <46024202+benlex5@users.noreply.github.com>
2020-06-09 12:57:00 +0100

    Added Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+import 'app/locator.dart';
+import 'app/router.dart';
 
 void main() {
   runApp(App());
--- lib/ui/views/startup/startup_viewmodel.dart
+++ lib/ui/views/startup/startup_viewmodel.dart
@@ -1,8 +1,10 @@
+import 'package:recipe_cook_book/app/locator.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class StartupViewModel extends BaseViewModel {
   final NavigationService _navigationService = locator<NavigationService>();
+  
   // TODO:
   //    loading, 
   //    authentication, 
